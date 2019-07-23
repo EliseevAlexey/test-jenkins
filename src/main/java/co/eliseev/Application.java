cdd314b3603518517e80847c4a31d87351222132
Alexey Eliseev <eliseev.co@gmail.com>
2019-07-23 12:39:37 +0300

    init

---
+++ src/main/java/co/eliseev/Application.java
@@ -0,0 +1,7 @@
+package co.eliseev;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/test/java/co/eliseev/ApplicationTest.java
@@ -0,0 +1,12 @@
+package co.eliseev;
+
+import org.junit.Test;
+
+
+public class ApplicationTest {
+
+    @Test
+    public void test() {
+        System.out.println("Hello from test");
+    }
+}
